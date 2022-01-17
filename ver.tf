5d84114837e863c2e17d84f0630bd1e62cfb7587
sushanthkumar114 <sushanthkumar1026@gmail.com>
2022-01-17 11:53:52 +0000

    22

---
+++ ver.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.7"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = default
+  shared_credentials_file = var.credentials
+}
